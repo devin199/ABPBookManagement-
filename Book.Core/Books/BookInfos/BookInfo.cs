8c4369ae47c2eb990d6378554f1decfcdd0e48b4
981171660 <981171660@qq.com>
2018-04-15 16:53:07 +0800

    为了测试分支，进行的一次提交

---
+++ Book.Core/Books/BookInfos/BookInfo.cs
@@ -0,0 +1,15 @@
+﻿
+using Abp;
+using Abp.Domain.Entities.Auditing;
+
+namespace Book.Books.BookInfos
+{
+   public class BookInfo:FullAuditedEntity
+    {
+        public string BookName { get; set; }
+
+        public string Author { get; set; }
+
+        public int BookNumber { get; set; }
+    }
+}

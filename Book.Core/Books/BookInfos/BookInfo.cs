46cb318ef5f0added273280b78b954bdc7442b48
981171660 <981171660@qq.com>
2018-04-15 16:57:11 +0800

    测试分支

--- Book.Core/Books/BookInfos/BookInfo.cs
+++ Book.Core/Books/BookInfos/BookInfo.cs
@@ -6,10 +6,19 @@ namespace Book.Books.BookInfos
 {
    public class BookInfo:FullAuditedEntity
     {
+        /// <summary>
+        /// 书名
+        /// </summary>
         public string BookName { get; set; }
 
+        /// <summary>
+        /// 作者
+        /// </summary>
         public string Author { get; set; }
-
-        public int BookNumber { get; set; }
+        
+        /// <summary>
+        /// 书籍数量
+        /// </summary>
+        public int BookNumberasd { get; set; }
     }
 }
